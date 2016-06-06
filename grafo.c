66b89edab1d438d298985e52f1140746213b34cb
Vytor Calixto <vytorcalixto@gmail.com>
2016-06-05 23:02:15 -0300

    Vemk

--- grafo.c
+++ grafo.c
@@ -6,7 +6,7 @@
 #include "lista.h"
 
 typedef struct rotulo{
-    int tamanho;
+    unsigned int tamanho;
     int* rtl;
 } *rotulo;
 
@@ -23,24 +23,26 @@ typedef struct vertice{
     unsigned int grau_ent; // Entrada dos direcionados
     unsigned int grau_sai; // Saída dos direcionados
     rotulo rtl;
+    int visitado;
 
     lista arestas;
 };
 
 typedef struct aresta{
     long int peso;
-    vertice *destino;
+    vertice destino;
 } *aresta;
 
 rotulo constroi_rotulo() {
     rotulo r = malloc(sizeof(struct rotulo));
+    if(!r) {puts("NULL");return NULL;}
     r->tamanho = 0;
-    r->rtl = calloc(1, sizeof(int));
+    r->rtl = malloc(sizeof(int)*256);
     return r;
 }
 
 void insere_rotulo(rotulo r, int n) {
-    r->rtl = realloc(r->rtl, sizeof(int)*r->tamanho+1);
+    // r->rtl = realloc(r->rtl, sizeof(int)*((r->tamanho)+1));
     r->rtl[r->tamanho] = n;
     ++r->tamanho;
 }
@@ -52,8 +54,8 @@ void destroi_rotulo(rotulo r) {
 
 // Retorna 1 se a > b, 0 caso contrário
 int compara_rotulo(rotulo a, rotulo b) {
-    if(a->tamanho = 0) return 0;
-    if(b->tamanho = 0) return 1;
+    if(b->tamanho == 0) return 1;
+    if(a->tamanho == 0) return 0;
     int min = (a->tamanho < b->tamanho) ? a->tamanho : b->tamanho;
     int i = 0;
     while(i < min) {
@@ -65,18 +67,36 @@ int compara_rotulo(rotulo a, rotulo b) {
     return (a->tamanho < b->tamanho) ? a->tamanho : b->tamanho;
 }
 
-vertice maior_rotulo(lista l) {
-    no n = primeiro_no(l);
-    vertice v = (vertice) conteudo(n);
-    vertice maior = v;
-    n = proximo_no(n);
+no maior_rotulo(lista l) {
+    no n;
+    vertice v;
+    for(n = primeiro_no(l); n; n = proximo_no(n)) {
+        v = (vertice) conteudo(n);
+        if(!v->visitado) {
+            break;
+        }
+    }
+    // if(!n) {
+    //     return NULL;
+    // }
+    printf("primeiro: %s rótulo:", nome_vertice((vertice) conteudo(n)));
+    for(int j=0; j < ((vertice) conteudo(n))->rtl->tamanho; ++j) {
+        printf("%d\t", ((vertice) conteudo(n))->rtl->rtl[j]);
+    }
+    puts("");
+    no maior = n;
     while(n) {
         vertice w = (vertice) conteudo(n);
-        if(!compara_rotulo(v->rtl, w->rtl)) {
-            maior = w;
+        if(!compara_rotulo(v->rtl, w->rtl) && !w->visitado) {
+            maior = n;
         }
         n = proximo_no(n);
     }
+    printf("Maior: %s Rótulo:", nome_vertice((vertice) conteudo(maior)));
+    for(int j=0; j < ((vertice) conteudo(maior))->rtl->tamanho; ++j) {
+        printf("%d\t", ((vertice) conteudo(maior))->rtl->rtl[j]);
+    }
+    puts("");
     return maior;
 }
 
@@ -139,6 +159,7 @@ grafo le_grafo(FILE *input){
         v->grau_sai = (unsigned int) agdegree(g, n, FALSE, TRUE);
         v->arestas = constroi_lista();
         v->rtl = constroi_rotulo();
+        v->visitado = 0;
         insere_lista(v, gf->vertices);
     }
 
@@ -194,6 +215,7 @@ void destroi_referencias(vertice v, grafo g) {
         for(no p = primeiro_no(w->arestas); p; p = proximo_no(p)) {
             aresta a = (aresta) conteudo(p);
             if(strcmp(nome_vertice(a->destino), nome_vertice(v)) == 0) {
+                printf("removendo %s da aresta com o vértice %s\n", v->nome, w->nome);
                 remove_no(w->arestas, p, *destroi_aresta);
             }
         }
@@ -248,6 +270,11 @@ grafo copia_grafo(grafo g) {
         w->grau_ent = v->grau_ent;
         w->grau_sai = v->grau_sai;
         w->arestas = constroi_lista();
+        w->rtl = constroi_rotulo();
+        w->rtl->tamanho = v->rtl->tamanho;
+        for(int i=0; i < v->rtl->tamanho; ++i) {
+            w->rtl->rtl[i] = v->rtl->rtl[i];
+        }
         insere_lista(w, gf->vertices);
     }
 
@@ -397,10 +424,39 @@ lista busca_largura_lexicografica(grafo g) {
     no n = primeiro_no(h->vertices);
     vertice v = (vertice) conteudo(n);
     insere_rotulo(v->rtl, tamanho_lista(h->vertices));
-    insere_lista(v, busca);
-    //TODO: pegar o vizinho com maior rótulo para ser o próximo
+    int i = tamanho_lista(h->vertices);
 
-    return constroi_lista();
+    puts("");
+    while(i > 0) {
+        puts("-----------");
+        printf("v: %s rótulo:", nome_vertice(v));
+        for(int j=0; j < v->rtl->tamanho; ++j) {
+            printf("%d\t", v->rtl->rtl[j]);
+        }
+        puts("");
+        lista vizinhos = vizinhanca(v, 0, h);
+        escreve_grafo(stdout,h);
+        puts("Vizinhos");
+        for(no m = primeiro_no(vizinhos); m; m = proximo_no(m)) {
+            vertice w = (vertice) conteudo(m);
+            printf("w: %s visitado: %d rótulo:", nome_vertice(w), w->visitado);
+            insere_rotulo(w->rtl, i-1);
+            for(int j=0; j < w->rtl->tamanho; ++j) {
+                printf("%d\t", w->rtl->rtl[j]);
+            }
+            puts("");
+        }
+        v->visitado=1;
+        insere_lista(v, busca);
+        n = maior_rotulo(h->vertices);
+        // if(!n) {
+        //     break;
+        // }
+        v = (vertice) conteudo(n);
+        --i;
+    }
+
+    return busca;
 }
 
 //------------------------------------------------------------------------------
@@ -411,6 +467,31 @@ lista busca_largura_lexicografica(grafo g) {
 // o tempo de execução é O(|V(G)|+|E(G)|)
 
 int ordem_perfeita_eliminacao(lista l, grafo g) {
+    no n = primeiro_no(l);
+    vertice v = (vertice) conteudo(n);
+
+    do {
+        int i = 0;
+        lista vizinhos = vizinhanca(v, 0, g);
+        vertice vizinhos_lista[tamanho_lista(vizinhos)];
+        no m = proximo_no(n);
+        do {
+            vertice w = (vertice) conteudo(m);
+            if(pertence(*compara_vertice, w, vizinhos)) {
+                vizinhos_lista[i] = w;
+                ++i;
+            }
+            m = proximo_no(m);
+        } while(m);
+        lista vizinhos_0 = vizinhanca(vizinhos_lista[0], 0, g);
+        for(int j=1; j<i; ++j) {
+            if(!pertence(*compara_vertice, vizinhos_lista[j], vizinhos_0)) return 0;
+        }
+        no next = proximo_no(n);
+        remove_no(l, n, NULL);
+        n = next;
+    } while(tamanho_lista(l) > 0);
+
     return 1;
 }
 
@@ -419,23 +500,31 @@ int ordem_perfeita_eliminacao(lista l, grafo g) {
 //         0, caso contrário
 
 int cordal(grafo g){
-    int eh_cordal = 1;
+    // int eh_cordal = 1;
+    //
+    // grafo novo_grafo = copia_grafo(g); // Copia o grafo
+    //
+    // no n = primeiro_no(novo_grafo->vertices);
+    // while(tamanho_lista(novo_grafo->vertices) > 0 && n) {
+    //     vertice v = (vertice) conteudo(n);
+    //     if(simplicial(v, novo_grafo)) {
+    //         destroi_referencias(v, novo_grafo);
+    //         remove_no(novo_grafo->vertices, n, *destroi_vertice);
+    //         n = primeiro_no(novo_grafo->vertices);
+    //         eh_cordal = 1;
+    //     } else {
+    //         n = proximo_no(n);
+    //         eh_cordal = 0;
+    //     }
+    // }
+    // destroi_grafo(novo_grafo);
+    // return eh_cordal; // Xablau
 
-    grafo novo_grafo = copia_grafo(g); // Copia o grafo
-
-    no n = primeiro_no(novo_grafo->vertices);
-    while(tamanho_lista(novo_grafo->vertices) > 0 && n) {
-        vertice v = (vertice) conteudo(n);
-        if(simplicial(v, novo_grafo)) {
-            destroi_referencias(v, novo_grafo);
-            remove_no(novo_grafo->vertices, n, *destroi_vertice);
-            n = primeiro_no(novo_grafo->vertices);
-            eh_cordal = 1;
-        } else {
-            n = proximo_no(n);
-            eh_cordal = 0;
-        }
+    lista busca = busca_largura_lexicografica(g);
+    puts("");
+    for(no n = primeiro_no(busca); n; n = proximo_no(n)) {
+        printf("%s\t", nome_vertice((vertice) conteudo(n)));
     }
-    destroi_grafo(novo_grafo);
-    return eh_cordal; // Xablau
+    return 1;
+    // return ordem_perfeita_eliminacao(busca, g);
 }
